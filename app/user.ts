60c4f2c58590fc0cf26ca6befe5bbe7ae33a137d
gabbers81 <gabriel.garrido18@gmail.com>
2016-10-28 10:00:52 -0400

    Creating User class

---
+++ app/user.ts
@@ -0,0 +1,13 @@
+export class User {
+	{
+		 name: string;
+		 email: string,
+		 phone: string,
+		 address: {
+			street: string,
+			apt: string,
+			city: string,
+			zipcode: string,
+		}
+	}
+}
